4202c3500d9eca6ea721d9c05174014f7bc410f9
devopsvikramsingh <vikram.singhuk08@outlook.com>
2025-09-30 10:00:34 +0530

    create currency conveter

---
+++ lib/currency_conveter_material_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+//1.create a variable for stores the converted amount
+//2.create a variable for stores the input amount
+//3.store the value in the variable that we created
+//4.display the variable
+
+class CurrencyConverterMaterialPage extends StatefulWidget {
+  const CurrencyConverterMaterialPage({super.key});
+  
+@override
+  State<CurrencyConverterMaterialPage> createState() =>
+      _CurrencyConverterMaterialPageState();
+  
+}
+
+class _CurrencyConverterMaterialPageState
+    extends State<CurrencyConverterMaterialPage> {
+  double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
+
+
+
+  @override
+ Widget build(BuildContext context) { //1.create a variable for stores the converted amount
+    return Scaffold(
+      backgroundColor: Colors.cyan,
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+        elevation: 0,
+        title: const Text(
+          'Currency Converter',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              //int ->string integervalue.toString() agr kisi  cheej ko string m convert karna hoga to tostring method call karte hain
+              //string -> int int.parse(stringvalue) agr kisi cheej ko int m convert karna hoga to int.parse method call karte hain
+              result.toString(),
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: textEditingController,
+                style: TextStyle(fontSize: 15, color: Colors.black),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  hintText: 'Please enter the amount in USD',
+                  hintStyle: TextStyle(color: Colors.black),
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ),
+            ),
+            // ElevatedButton
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  
+                  setState(() {
+                    result = (double.parse(textEditingController.text) * 82);
+                  });
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 50),
+
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+
+                child: const Text('Convert'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
